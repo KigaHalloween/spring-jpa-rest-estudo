b8a7618e129860026aa95f0700735a34eccd22fb
Kiga Halloween <andrefelipe644@gmail.com>
2023-09-04 21:01:02 -0300

    commit 2.1

--- src/main/java/projeto/spring/vendasApplication.java
+++ src/main/java/projeto/spring/vendasApplication.java
@@ -4,14 +4,10 @@ package projeto.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class vendasApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(vendasApplication.class, args);
-
-
     }
 }
